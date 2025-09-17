c3e652171686de4c6fababa960daf1914e2ce24d
Roberto Rodriguez <prfiredragon@gmail.com>
2025-09-17 12:26:37 -0400

    prepare for upload

--- webformd/src/lib.rs
+++ webformd/src/lib.rs
@@ -1,4 +1,4 @@
-// In your `oursistem` or shared library crate.
+
 pub trait WebFomData: Sized {
     /// Deserializes a struct from a `Vec<(String, String)>`.
     fn deserialize(data: &Vec<(String, String)>) -> Result<Self, String>;
